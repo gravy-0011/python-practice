ecc8a4af1cb70b441a6050d1ed2f7fc59f3c594b
gravy-0011 <gavyarora90@gmail.com>
2026-07-11 12:21:33 +0530

    add basicpractice.py

---
+++ basicpractice.py
@@ -0,0 +1,21 @@
+#Print your name.
+print("gravy")
+#Print your name, age, and city on separate lines.
+print("my name is gravy\ni am 18 years old\ni am from fatehgarh pam" \
+"njtoor")
+#Store your name in a variable and print:
+name="gravy"
+print(name)
+#Store two numbers and print their sum.
+a=7
+b=9
+sum=a+b
+print("sum:", sum)
+#Swap two numbers without using a third variable.
+a=7
+b=9
+print("Before swapping: a =", a, "b =", b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping: a =", a, "b =", b)
